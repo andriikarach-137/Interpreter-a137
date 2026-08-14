eb12dcf3612dc66ae8194a64de0602c88cceb2be
Andrii Karach <andrii.karach.13@gmail.com>
2026-08-14 22:45:07 +0100

    Finish BNF

--- Parser.hs
+++ Parser.hs
@@ -13,8 +13,14 @@ import ParseUtils
 
 <assign>     ::= <type> <ident> LARR <expr>
 <print>      ::= PRINT <expr> 
+<type>       ::= <type_atom> <type_arrow>
+<type_atom>  ::= INT | REAL | CHAR | STRING | LSB <type> RSB | LB <type> COM <type> RB |
+                 LCB <type> DRARR <type> RCB
+<type_arrow> ::= RARR <type> | eps 
 
-<expr>       ::= <or> | <or> CONS <expr> 
+<expr>       ::= <cons> <ternary> 
+<ternary>    ::= QM <expr> CLN <expr> | eps 
+<cons>       ::= <or> | <or> DCLN <cons> 
 <or>         ::= <xor> (OR <xor>)*
 <xor>        ::= <and> (XOR <and>)* 
 <and>        ::= <comp> (AND <comp>)* 
@@ -28,7 +34,7 @@ import ParseUtils
                  TAIL | FST | SND 
 <apply>      ::= <postfix> (APP <postfix>)* 
 <postfix>    ::= <primary> (DPLUS | DMINUS | EXCL)* 
-<primary>    ::= <ident> LARR <expr> | <literal> | LBR <expr> RBR 
+<primary>    ::= <ident> | <literal> | LBR <expr> RBR 
 <ident>      ::= <alpha_util> (<alnum_util>)* 
 <literal>    ::= <int_lit> | <real_lit> | <char_lit> | <string_lit> | <pair_lit> | 
                  <list_lit> | <dict_lit> | <fun_lit>
@@ -39,21 +45,72 @@ import ParseUtils
 <pair_lit>   ::= LB <expr> COM <expr> RB
 <list_lit>   ::= LSB ((<expr> (COM <expr>)*) | eps) RSB
 <dict_lit>   ::= LCB ((<dict_entry> (COM <dict_entry>)*) | eps) RCB
-<dict_entry> ::= <expr> CLN <expr>
+<dict_entry> ::= <expr> DRARR <expr>
 <fun_lit>    ::= BSLASH <ident> RARR <expr>
 
 
-Utility parsers:
+Utility parsers: @f denotes utility parser constructed as satisfy f 
 
-<num_util>   ::= (<digit>)+ 
-<digit>      ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+<num_util>   ::= (<digit>)+
+<digit>      ::= @isDigit 
 
 <real_util>  ::= <num_util> DOT <num_util> 
 
-<item_util>  ::= ANY_CHAR 
+<item_util>  ::= @(\c -> c /= '\'' && c /= '\"')
 
-<alpha_util> ::= LETTER 
+<alpha_util> ::= @(isAlpha) 
 
-<alnum_util> ::= <alpha_util> | <digit>
+<alnum_util> ::= @(isAlphaNum)
 
+
+Terminals:
+
+SEMI         ::= ";"
+LARR         ::= "<-"
+PRINT        ::= "print"
+CLN          ::= ":"
+OR           ::= "||"
+XOR          ::= "^^"
+AND          ::= "&&"
+EQ           ::= "="
+NEQ          ::= "/="
+LT           ::= "<"
+GT           ::= ">"
+LE           ::= "<="
+GE           ::= ">="
+DPLUS        ::= "++"
+PLUS         ::= "+"
+MINUS        ::= "-"
+STAR         ::= "*"
+SLASH        ::= "/"
+DSTAR        ::= "**"
+EXCL         ::= "!"
+LOG          ::= "log"
+EXP          ::= "exp"
+SIN          ::= "sin"
+COS          ::= "cos"
+TAN          ::= "tan"
+ASIN         ::= "asin"
+ACOS         ::= "acos"
+ATAN         ::= "atan"
+HEAD         ::= "head"
+TAIL         ::= "tail"
+FST          ::= "fst"
+SND          ::= "snd"
+APP          ::= "$"
+DMINUS       ::= "--"
+RARR         ::= "->"
+LBR          ::= "("
+RBR          ::= ")"
+SQ           ::= "'"
+DQ           ::= "\""
+LSB          ::= "["
+RSB          ::= "]"
+LCB          ::= "{"
+RCB          ::= "}"
+COM          ::= ","
+BSLASH       ::= "\"
+DRARR        ::= "=>"
+QM           ::= "?"
+DCLN         ::= "::"
 -}
